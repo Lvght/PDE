23acf8b00c93d4727f6fd213c8751dea0fda18c2
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2024-08-02 14:37:57 -0300

    feat: add stream logs

--- datastreamer/stream.py
+++ datastreamer/stream.py
@@ -6,11 +6,18 @@ import time
 
 async def stream_data(websocket, path):
     data = pd.read_csv('data.csv')
-
+    print("Data stream started")
     for _, row in data.iterrows():
-        print(f'Seding data {row.to_json()}')
-        await websocket.send(row.to_json())
-        await asyncio.sleep(1)  # Stream each item every 1 second
+        try:
+            print(f'Seding data {row.to_json()}')
+            await websocket.send(row.to_json())
+            await asyncio.sleep(1)  # Stream each item every 1 second
+        except websockets.exceptions.ConnectionClosedError as e:
+            print(f'Connection closed: {e}')
+        except Exception as e:
+            print(f'Error: {e}')
+
+    print('Data stream completed')
 
 
 async def main():
